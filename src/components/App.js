5e364b28b108a0b08fedd64d85cf1b3cfb06a790
Davanjit Sandhu <davanjit@hotmail.com>
2022-10-13 22:17:03 -0400

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,43 @@ import Settings from './Settings/Settings'
 import { store } from '../store'
 import ListMaker from './ListMaker/ListMaker'
 import Items from './Items/Items'
+import Navbar from './Navbar/Navbar'
+import { useId } from 'react'
 
 function App() {
+
+  const elements = [
+    {
+      name: "Add Item",
+      class: "nav-add",
+      id: useId(),
+      flag: true,
+      element: <ListMaker />
+    },
+    {
+      name: "Save List",
+      class: "nav-save",
+      id: useId(),
+      flag: false
+    },
+    {
+      name: "Load List",
+      class: "nav-load",
+      id: useId(),
+      flag: false
+    },
+    {
+      name: "Settings",
+      class: "nav-settings",
+      id: useId(),
+      flag: true,
+      element: <Settings />
+    }
+  ]
+
   return (
     <Provider store={ store }>
-      <Settings />
-      <ListMaker />
+      <Navbar ELEMENTS={elements} />
       <Items />
     </Provider>
   )
