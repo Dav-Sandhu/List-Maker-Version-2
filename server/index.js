7a9edd49f2dc06d20808c83faecc08d12c126d85
Davanjit Sandhu <davanjit@hotmail.com>
2022-10-17 05:46:53 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,44 +1,73 @@
-import mongoose from 'mongoose'
 import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+import path from 'path'
+import mongoose from 'mongoose'
 import { readFileSync } from 'fs'
-import { dirname, join } from 'path'
 import { fileURLToPath } from 'url'
-import Item from './item.js'
+import List from './list.js'
 import cors from 'cors'
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const port = process.env.PORT || 8000
 
-const data = JSON.parse(readFileSync(join(__dirname, "data.json"), "utf-8"))
-const dbURI = "mongodb+srv://" + data.username + ":" + data.password + 
-	"@project-backend.grxkvf1.mongodb.net/?retryWrites=true&w=majority"
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+const userData = JSON.parse(readFileSync(path.join(__dirname, "data.json"), "utf-8"))
+
+const dbURI = "mongodb+srv://" + userData.username + ":" + userData.password + 
+	"@project-backend.grxkvf1.mongodb.net/ListData?retryWrites=true&w=majority"
+
+const insertList = async (list, name) => {
+	
+	let exists = 0
+	
+	const l = new List({
+		list: list,
+		name: name
+	})
+	
+	await List.find({name: name})
+		.then(result => exists = result.length)
+	
+	if (exists === 0){
+		l.save().catch(err => console.log(err))
+	}else if (exists > 0){
+		List.findOneAndUpdate({name: name}, 
+		{list: list}).catch(err => console.log(err))	
+	}
+}
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.get("/", (req, res) => {
-	Item.find().then((response) => {
-		res.send(response)
-	}).catch(err => console.log(err))
+app.get('/', (req, res) => {
+  List.find().then((result) => res.send(result))
 })
 
-app.put("/", (req, res) => {
+app.get('/add-list/:list/:name/', (req, res) => {
+	insertList(JSON.parse(req.params.list), req.params.name)
+	res.redirect('/')
+})
+
+app.get('/remove-list/:rank/', (req, res) => {
+	List.deleteOne({rank: parseInt(req.params.rank)})
+		.then(() => {
+			res.redirect('/')
+		}).catch(err => console.log(err))
+})
+
+app.post('/', (req, res) => {
 	if (req.body.type === "insert"){
-		const i = new Item({
-			name: req.body.name,
-			date: req.body.date,
-			image: req.body.image,
-			shape: req.body.shape,
-			rank: parseInt(req.body.rank)
-		})
-		
-		i.save().catch(err => console.log(err))
+		insertList(JSON.parse(req.body.list), req.body.name)
+		res.send("request received!")
 	}else{
-		Item.deleteOne({rank: parseInt(req.body.rank)})
+		List.deleteOne({rank: parseInt(req.body.rank)})
+			.catch(err => res.send(err))
 	}
 })
 
-mongoose.connect(dbURI).then(() => {
-	app.listen(8000)
-}).catch(err => console.log(err))
+mongoose.connect(dbURI)
+	.then(result => app.listen(port))
+	.catch(err => console.log(err))
