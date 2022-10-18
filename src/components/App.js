1559d97258581221b7ccda40e5b7460f922f6382
Davanjit Sandhu <davanjit@hotmail.com>
2022-10-18 02:21:37 -0400

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,9 @@ import { Provider } from 'react-redux'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 import { TouchBackend } from 'react-dnd-touch-backend'
 import { DndProvider } from 'react-dnd'
-import Settings from './Settings/Settings'
 import { store } from '../store'
-import ListMaker from './ListMaker/ListMaker'
 import Items from './Items/Items'
-import Navbar from './Navbar/Navbar'
-import { useId } from 'react'
-import StoredLists from './StoredLists/StoredLists'
+import Navigation from './Navbar/Navigation'
 
 const isTouchDevice = () => {  
   return (('ontouchstart' in window) ||  
@@ -20,39 +16,9 @@ function App() {
 
   const mobile = isTouchDevice()
 
-  const elements = [
-    {
-      name: "Add Item",
-      class: "nav-add",
-      id: useId(),
-      flag: true,
-      element: <ListMaker />
-    },
-    {
-      name: "Save List",
-      class: "nav-save",
-      id: useId(),
-      flag: false
-    },
-    {
-      name: "Load List",
-      class: "nav-load",
-      id: useId(),
-      flag: true,
-      element: <StoredLists />
-    },
-    {
-      name: "Settings",
-      class: "nav-settings",
-      id: useId(),
-      flag: true,
-      element: <Settings />
-    }
-  ]
-
   return (
     <Provider store={ store }>
-      <Navbar ELEMENTS={elements} />
+      <Navigation />
       
       <DndProvider backend={mobile ? TouchBackend : HTML5Backend}>
         <Items />
