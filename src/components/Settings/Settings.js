ace1a04b998c611b74723765a47f3189134ed024
Davanjit Sandhu <davanjit@hotmail.com>
2022-10-18 02:23:38 -0400

    Update Settings.js

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,14 +1,25 @@
 import { useDispatch, useSelector } from "react-redux"
-import { setDarkMode, setOrder, setItemsPerPage } from "../../reducers/settingsSlice"
+import { setDarkMode, setOrder } from "../../reducers/settingsSlice"
 import { sortItems } from "../../reducers/itemsSlice"
 import './Settings.scss'
 
 const Settings = () => {
     const dispatch = useDispatch()
-    const { darkMode, order, itemsPerPage } = useSelector(state => state.settings)
+    const { darkMode, order } = useSelector(state => state.settings)
 
     return(
         <div className="settings"> 
+
+            <div className="darkMode">
+                <label htmlFor="darkMode">Dark Mode: </label>
+                <input 
+                    type="checkbox"
+                    data-testid="darkMode"
+                    id="darkMode"
+                    checked={darkMode}
+                    onChange={() => dispatch(setDarkMode())} />
+            </div>
+
             <label htmlFor="order">Order: </label>
             <select 
                 id="order"
@@ -22,30 +33,6 @@ const Settings = () => {
                 <option value="desc">Descending</option>
                 <option value="rand">Random</option>
             </select>
-
-            <div className="darkMode">
-                <label htmlFor="darkMode">Dark Mode: </label>
-                <input 
-                    type="checkbox"
-                    data-testid="darkMode"
-                    id="darkMode"
-                    checked={darkMode}
-                    onChange={() => dispatch(setDarkMode())} />
-            </div>
-
-            <div className="itemsPerPage">
-                <label htmlFor='itemsPerPage'>Items per page: </label>
-                <select 
-                    id="itemsPerPage"
-                    data-testid="itemsPerPage"
-                    value={itemsPerPage}
-                    onChange={(e) => dispatch(setItemsPerPage(e.target.value))}>
-                    <option value="25">25</option>
-                    <option value="50">50</option>
-                    <option value="100">100</option>
-                    <option value="999">999</option>
-                </select>
-            </div>
         </div>
     )
 }
