51e278d9b5877127e5b129e4fa65860ec672b1ff
Davanjit Sandhu <davanjit@hotmail.com>
2022-10-18 02:22:38 -0400

    Update StoredLists.js

--- src/components/StoredLists/StoredLists.js
+++ src/components/StoredLists/StoredLists.js
@@ -2,7 +2,7 @@ import { useSelector, useDispatch } from "react-redux"
 import { loadNewList, setTitle, sortItems } from "../../reducers/itemsSlice"
 import "./StoredLists.scss"
 
-const StoredLists = () => {
+const StoredLists = ({deleteList}) => {
 
   const dispatch = useDispatch()
 
@@ -10,17 +10,23 @@ const StoredLists = () => {
   const loadedLists = useSelector(state => state.items.loadedLists)
   const order = useSelector(state => state.settings.order)
 
+
   return(
     <div className={loadedNames.length > 0 ? "stored-lists" :  ""}>
       {loadedNames.map(l => {
         return(
-          <button 
-            key={l}
-            onClick={() => {
-              dispatch(loadNewList(loadedLists[loadedNames.indexOf(l)]))
-              dispatch(setTitle(l))
-              dispatch(sortItems(order))
-            }}>{l}</button>
+          <div key={l}>
+            <button
+              className="load-button" 
+              onClick={() => {
+                dispatch(loadNewList(loadedLists[loadedNames.indexOf(l)]))
+                dispatch(setTitle(l))
+                dispatch(sortItems(order))
+              }}>{l}</button>
+            <button
+              className="remove-button" 
+              onClick={() => deleteList(l)}>✖</button>
+          </div>
         )
       })}
     </div>
